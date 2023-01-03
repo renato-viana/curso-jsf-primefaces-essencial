41a6816ad84544afc88fa45dbf2d31c78e2d7961
Renato Viana <renatoviana30@gmail.com>
2023-01-03 14:57:14 -0300

    Implementando a edição e exclusão de registros

--- src/main/java/com/renatoviana/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/renatoviana/erp/controller/GestaoEmpresasBean.java
@@ -49,18 +49,28 @@ public class GestaoEmpresasBean implements Serializable {
 		empresa = new Empresa();
 	}
 
+	public void prepararEdicao() {
+		ramoAtividadeConverter = new RamoAtividadeConverter(Arrays.asList(empresa.getRamoAtividade()));
+	}
+
 	public void salvar() {
 		cadastroEmpresaService.salvar(empresa);
 
-		if (jaHouvePesquisa()) {
-			pesquisar();
-		} else {
-			todasEmpresas();
-		}
+		atualizarRegistros();
 
 		messages.info("Empresa salva com sucesso!");
 
 		RequestContext.getCurrentInstance().update(Arrays.asList("frm:empresasDataTable", "frm:messages"));
+	}
+
+	public void excluir() {
+		cadastroEmpresaService.excluir(empresa);
+
+		empresa = null;
+
+		atualizarRegistros();
+
+		messages.info("Empresa excluída com sucesso!");
 	}
 
 	public void pesquisar() {
@@ -81,6 +91,14 @@ public class GestaoEmpresasBean implements Serializable {
 		ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
 
 		return listaRamoAtividades;
+	}
+
+	private void atualizarRegistros() {
+		if (jaHouvePesquisa()) {
+			pesquisar();
+		} else {
+			todasEmpresas();
+		}
 	}
 
 	private boolean jaHouvePesquisa() {
@@ -109,6 +127,14 @@ public class GestaoEmpresasBean implements Serializable {
 
 	public Empresa getEmpresa() {
 		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public boolean isEmpresaSelecionada() {
+		return empresa != null && empresa.getId() != null;
 	}
 
 }
